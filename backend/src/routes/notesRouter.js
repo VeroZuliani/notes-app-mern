298b0b116c486edc8f844285ebe026cda206fc53
VeroZuliani <vero.zuliani92@gmail.com>
2026-04-28 18:36:46 -0300

    Realice conexion con MongoDB, y modifique rutas con HTTP status

--- backend/src/routes/notesRouter.js
+++ backend/src/routes/notesRouter.js
@@ -1,46 +1,81 @@
 import express from "express"
+import Note from "../models/noteModel.js"
 
 const router = express.Router()
 
 
 //Obtener todas las notas
-router.get('/', () => {
-    console.log("Enviando las notas")
+router.get('/', async (req, res) => {
+    try {
+        const notes = await Note.find()
+        res.status(200).json(notes)
+    } catch (error) {
+        console.log("Error al obtener las notas", error)
+        res.status(500).json({erro:"Internal server error"})   
+    }
 })
 
 
 //Obterner una nota por ID
-router.get('/:id', (req, res) => {
-    const id = req.params.id
-    console.log(req.params)
-    console.log(id)
-    console.log("Enviando una nota por ID")
+router.get('/:id', async (req, res) => {
+    try {
+        const id = req.params.id
+        const note = await Note.findById(id)
+        if(!note) 
+            return res.status(400).json({error:"Nota no encontrada"})
+        res.status(200).json({note})
+    } catch (error) {
+        console.log("Error al obtener nota por ID")
+        res.status(500).json({error: "Internal server error"})
+    }
 })
 
 
 //Crear una nueva nota
-router.post('/', (req, res) => {
-    const {title, description} = req.body
-    console.log(req.body) 
-    console.log(title, description)
+router.post('/', async(req, res) => {
+    try {
+        const {title, description} = req.body
+        const note = new Note({ title, description })
+    
+        const savedNote = await note.save()
+        if(savedNote){
+            res.status(201).json({message: "Note creada correctamente", note:savedNote})
+        }
+    } catch (error) {
+        console.error("Error al crear la nota", error)
+        res.status(500).json({error: "Internal server error"})
+    }
 })
 
 
 //Eliminar una nota
-router.delete('/:id', (req, res) => {
-    const id = req.params.id
-    console.log(id)
-    console.log('Eliminar una nota por ID')
+router.delete('/:id', async (req, res) => {
+    try {
+        const id = req.params.id
+        const deleteNote = await Note.findByIdAndDelete(id)
+        if(!deleteNote) 
+            return res.status(404).json({error: "Nota no eliminada"})
+        res.status(200).json({message: "Nota eliminada correctamente"})
+    } catch (error) {
+        console.error("Error al eliminar una nota ", error)
+        res.status(500).json({error: "Internal server error"})
+    }
 })
 
 
 //Editar una nota por ID
-router.put('/:id', (req, res) => {
-    const id = req.params.id
-    const updateData = req.body
-    console.log(id)
-    console.log(updateData)
-    console.log("Editar una nota por ID")
+router.put('/:id', async (req, res) => {
+    try {
+        const id = req.params.id
+        const {title, description} = req.body
+        const updateNote = await Note.findByIdAndUpdate(id, {title, description}, {new: true})
+        if(!updateNote) 
+            return res.status(404).json({error: "Nota no actualizada correctamente"})
+        res.status(200).json({message: "Nota actualizada correctamente", note: updateNote})
+    } catch (error) {
+        console.error("Error al actualizar una nota ", error)
+        res.status(500).json({error: "Internal server error"})
+    }
 })
 
 
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import notesRouter from "./routes/notesRouter.js"
 import dotenv from "dotenv"
 dotenv.config()
+import { connectDB } from "./config/db.js"
 
 const app = express()
 
@@ -11,8 +12,13 @@ app.use("/app/notes", notesRouter)
 
 const PORT = process.env.PORT || 3001
 
-app.listen(PORT, () => {
-    console.log(`Servidor levantado en puerto http://localhost:${PORT}`)
+connectDB()
+.then(() => {
+    app.listen(PORT, () => {
+        console.log(`Servidor levantado en puerto http://localhost:${PORT}`)
+    })
 })
+
+
 
 
